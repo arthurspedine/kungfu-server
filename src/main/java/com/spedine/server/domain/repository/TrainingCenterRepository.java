0f0982af15f372fa78807236195a3e828b0f590c
Arthur Spedine <tucogamesytthegamesof@gmail.com>
2025-04-21 20:12:47 -0300

    hotfix: add findAllAsc method

--- src/main/java/com/spedine/server/domain/repository/TrainingCenterRepository.java
+++ src/main/java/com/spedine/server/domain/repository/TrainingCenterRepository.java
@@ -3,10 +3,14 @@ package com.spedine.server.domain.repository;
 import com.spedine.server.domain.entity.TrainingCenter;
 import com.spedine.server.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface TrainingCenterRepository extends JpaRepository<TrainingCenter, UUID> {
     List<TrainingCenter> findAllByTeacher(User teacher);
+
+    @Query("select t from TrainingCenter t order by t.name asc")
+    List<TrainingCenter> findAllAsc();
 }
